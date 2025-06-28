fc999c0280002b715b358cab12e88460ddc5104a
Isabella <119835878+mintylemon66@users.noreply.github.com>
2025-06-28 18:16:37 -0400

    Update Leaderboard.tsx

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -81,7 +81,7 @@ const Leaderboard = ({ currentRaceResults }: LeaderboardProps) => {
   const formatTime = (timeInMs: number) => {
     const seconds = Math.floor(timeInMs );
     const milliseconds = timeInMs % 1000;
-    return `${seconds}.${milliseconds.toString().padStart(3, '0')}s`;
+    return timeInMs;
   };
 
   return (
